40bfeef6f6af33675ee5b0406b90300c3a39ceb0
tomlion <qycpublic@gmail.com>
2011-09-20 17:38:53 +0800

    Replace js path with Engine root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 MailsViewer::Engine.routes.draw do
   controller "home" do
-    match "/", :to => :index
     get "/index", :to => :index
     get "/raw", :to => :raw
     get "/html", :to => :html
   end
+  root to: 'home#index'
 end
