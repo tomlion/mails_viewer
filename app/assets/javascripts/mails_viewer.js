34bbcad59570ea1b2a9c0f8b464bffdfdbdff804
tomlion <qycpublic@gmail.com>
2011-09-21 21:00:29 +0800

    dispatch static assets directly, didn't rely on sprockets

--- app/assets/javascripts/mails_viewer.js
+++
@@ -1,3 +0,0 @@
-//= require jquery
-//= require_tree .
-
--- lib/mails_viewer/engine.rb
+++ lib/mails_viewer/engine.rb
@@ -5,5 +5,10 @@ module MailsViewer
   class Engine < Rails::Engine
     engine_name = :mails_viewer
     isolate_namespace MailsViewer
+
+    initializer "static assets" do |app|
+      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
+    end
   end
+
 end
