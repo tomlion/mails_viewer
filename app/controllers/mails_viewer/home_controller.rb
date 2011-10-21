0ac39e30414bb30e837433d5ae44c4e7723136e2
Ian Yang <me@iany.me>
2011-10-21 20:20:04 +0800

    Use relative filename for mail to avoid security issue

--- app/controllers/mails_viewer/home_controller.rb
+++ app/controllers/mails_viewer/home_controller.rb
@@ -2,29 +2,39 @@ module MailsViewer
   class HomeController < ActionController::Base
     layout false
     before_filter :disabled_on_production
+    before_filter :find_absolute_filename, :only => [:raw, :html]
 
     def index
-      @files = Dir[mails_path+"/**/*"].select{|f| File.file?(f)}
-      @mails = @files.inject([]) do |mails, f|
-        mails << Mail.read(File.join(f)) 
+      Dir.chdir(mails_path) do
+        @files = Dir["**/*"].select{|f| File.file?(f)}
+        @mails = @files.inject([]) do |mails, f|
+          mails << Mail.read(File.join(f)) 
+        end
       end
     end
 
     def raw
-      render text: File.read(params[:filename])
+      if @filename
+        render text: File.read(@filename)
+      else
+        head :not_found
+      end
     end
 
     def html
-      mail = Mail.read(params[:filename])
-      render text: mail.html_part.body
+      if @filename
+        mail = Mail.read(@filename)
+        render text: mail.html_part.body
+      else
+        head :not_found
+      end
     end
 
 
   private
     def mails_path
-      path = Rails.application.config.action_mailer.file_settings.try(:[], :location) || './tmp/mails'
-      path = File.join(Rails.root, path) unless path.start_with?('/')
-      path
+      @mails_path ||= File.expand_path(Rails.application.config.action_mailer.file_settings.try(:[], :location) || './tmp/mails',
+                                       Rails.root)
     end
 
     def disabled_on_production
@@ -33,5 +43,16 @@ module MailsViewer
       end
     end
 
+    def find_absolute_filename
+      if params[:filename].present?
+        filename = File.expand_path(params[:filename], mails_path)
+
+        # ensure the file is in mails_path
+        if filename == File.join(mails_path, params[:filename])
+          @filename = filename
+        end
+      end
+    end
+    
   end
 end
