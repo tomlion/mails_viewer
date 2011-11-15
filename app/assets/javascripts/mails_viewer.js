33e3b75d45dda52a53e22c95af5584dee1e7571d
Jan Xie <jan.h.xie@gmail.com>
2011-11-15 14:01:00 +0800

    fix time sort; by default order in time desc

--- app/assets/javascripts/mails_viewer.js
+++ app/assets/javascripts/mails_viewer.js
@@ -1,3 +1,20 @@
 //= require jquery
 //= require_tree .
 
+$(function() {
+  $("table").tablesorter({
+    widthFixed: true,
+    sortList: [[3, 1]]
+  }).tablesorterPager({
+    container: $("#pager")
+  });
+
+  $('.mail a.html').colorbox({width:"80%", height:"80%", iframe:true});
+  $('.mail a.raw').click(function(){
+    $.get($(this).attr('href'), function(data){
+      $('#raw').text(data);
+    });
+    return false;
+  });
+
+});
