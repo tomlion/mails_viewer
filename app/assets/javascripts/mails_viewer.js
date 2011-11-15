61f088dd6422df47a24faed165ba89e7bdf72356
Jan Xie <jan.h.xie@gmail.com>
2011-11-15 14:46:05 +0800

    dynamic applying plugins to fix no colorbox and rawbox on later page bug

--- app/assets/javascripts/mails_viewer.js
+++ app/assets/javascripts/mails_viewer.js
@@ -2,19 +2,25 @@
 //= require_tree .
 
 $(function() {
+  $.livequery.registerPlugin("tablesorter", "tablesorterPager");
+
+  $('.mail a.html').livequery(function() {
+    console.log(this);
+    $(this).colorbox({width:"80%", height:"80%", iframe:true});
+  });
+
+  $('.mail a.raw').live('click', function(){
+    $.get($(this).attr('href'), function(data){
+      $('#raw').text(data);
+    });
+    return false;
+  });
+
   $("table").tablesorter({
     widthFixed: true,
     sortList: [[3, 1]]
   }).tablesorterPager({
     container: $("#pager")
-  });
-
-  $('.mail a.html').colorbox({width:"80%", height:"80%", iframe:true});
-  $('.mail a.raw').click(function(){
-    $.get($(this).attr('href'), function(data){
-      $('#raw').text(data);
-    });
-    return false;
   });
 
   var fix_top = parseInt($('#pager').css('top'), 10) + 60 + 'px';
