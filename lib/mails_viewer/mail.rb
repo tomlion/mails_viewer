b27fd13579f535390481995aa0c2e201d4c0c521
Cory Forsyth <cory.forsyth@gmail.com>
2012-09-28 18:52:05 -0400

    fix failing test

--- lib/mails_viewer/mail.rb
+++ lib/mails_viewer/mail.rb
@@ -1,9 +1,9 @@
 module Mail
   class Message
-    # Save the mail to seperate files when have several destination addresses
+    # Save the mail to separate files when have several destination addresses
     # This function used to return a filename by mail's date and destination address
     def filename_for(to)
-      "#{date.to_time.to_i}.#{to}.txt"
+      "#{Time.now.to_i}.#{to}.txt"
     end
   end
 end
